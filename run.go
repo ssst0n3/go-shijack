a544d3568c40ec498b0dbbd5113ca974f4759ff6
Lei Wang <ssst0n3@gmail.com>
2022-10-25 16:51:56 +0800

    init cli

--- run.go
+++ run.go
@@ -1,12 +1,5 @@
 package gohijack
 
-import "github.com/ssst0n3/awesome_libs/log"
-
-func Run(src, dst string, seq, ack uint32, reset bool) {
-	log.Logger.Infof("Using SEQ = 0x{%x}, ACK = 0x{%x}", seq, ack)
-	//connection := NewConnection(src, dst, seq, ack)
-	//if reset {
-	//	connection.Reset()
-	//	log.Logger.Info("Connection has been reset")
-	//}
+func Hijack(interfaceName string, srcIp string, srcPort uint, dstIp string, dstPort uint, payloadFile string) {
+	return
 }
