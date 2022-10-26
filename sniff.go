d199f6dbb6f8066be1d800728d3189459acbf35b
Lei Wang <ssst0n3@gmail.com>
2022-10-26 15:32:54 +0800

    implement PureGo sniffer

---
+++ sniff.go
@@ -0,0 +1,32 @@
+package gohijack
+
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
+	"github.com/ssst0n3/awesome_libs/awesome_error"
+	"golang.org/x/net/bpf"
+	"log"
+)
+
+type Sniffer interface {
+	Sniff(device string) (packets chan gopacket.Packet)
+}
+
+type PureGo struct {
+}
+
+func (s PureGo) Sniff(device string, filter []bpf.RawInstruction) (packets chan gopacket.Packet, err error) {
+	handle, err := pcapgo.NewEthernetHandle(device)
+	if err != nil {
+		awesome_error.CheckErr(err)
+		return
+	}
+	err = handle.SetBPF(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	packetSource := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	packets = packetSource.Packets()
+	return
+}
